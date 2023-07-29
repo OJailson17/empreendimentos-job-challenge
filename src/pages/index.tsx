271723926cb1bd0d834cb750cc14b5af6065fc8d
OJailson17 <JaylsonO17@gmail.com>
2023-07-29 17:25:56 -0300

    feat: add pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,92 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Enterprise } from '@/components/Enterprise';
 import { Header } from '@/components/Header';
 import { SearchBar } from '@/components/SearchBar';
 import { api } from '@/lib/axios';
 import { GetServerSideProps } from 'next';
-
-interface Enterprise {
-	id: string;
-	name: string;
-	status: string;
-	purpose: string;
-	address: {
-		district: string;
-		city: string;
-		street: string;
-		state: string;
-		number: string;
-		cep: string;
-	};
-}
+import { AppEnterprisesContainer, AppMainContainer } from './styles';
+import { Button } from '@/components/Button';
+import { useCallback, useEffect, useState } from 'react';
+import { useEnterprise } from '@/hooks/useEnterprise';
+import { EnterpriseProps } from '@/@types/Enterprise';
 
 interface HomeProps {
-	enterprises: Enterprise[];
+	enterprises_list: EnterpriseProps[];
 }
 
 interface Response {
-	data: Enterprise[];
+	data: EnterpriseProps[];
 }
 
-export default function Home({ enterprises }: HomeProps) {
+export default function Home({ enterprises_list }: HomeProps) {
+	// const [enterprises, setEnterprises] =
+	// 	useState<Enterprise[]>(enterprises_list);
+	const [page, setPage] = useState(1);
+
+	const { handleSetEnterprises, enterprises } = useEnterprise();
+
+	const handleLoadEnterprises = useCallback(async () => {
+		try {
+			const enterpriseResponse: Response = await api.get(
+				`/enterprises/?&_limit=2&_page=${String(page)}`,
+			);
+			const enterprisesData = enterpriseResponse.data;
+
+			const formatEnterprises = enterprises.concat(enterprisesData);
+
+			handleSetEnterprises(formatEnterprises);
+			console.log({ formatEnterprises });
+		} catch (error) {
+			console.log({ error });
+		}
+	}, [page]);
+
+	const handleAddPageCount = async () => {
+		setPage(currentPage => currentPage + 1);
+	};
+
+	useEffect(() => {
+		const fetchEnterprises = async () => {
+			await handleLoadEnterprises();
+		};
+
+		if (page > 1) {
+			fetchEnterprises();
+		}
+	}, [handleLoadEnterprises, page]);
+
+	useEffect(() => {
+		handleSetEnterprises(enterprises_list);
+	}, [enterprises_list]);
+
 	return (
 		<>
 			<Header />
 
-			{/* Search bar */}
 			<SearchBar />
 
-			{/* list */}
-			<main
-				style={{
-					padding: '0 8.5rem 0 10.25rem',
-					marginTop: '3.6875rem',
-					display: 'flex',
-					flexDirection: 'column',
-					gap: '2rem',
-				}}
-			>
-				<Enterprise />
-				<Enterprise />
-			</main>
+			<AppMainContainer>
+				<AppEnterprisesContainer>
+					{enterprises.map(enterprise => (
+						<Enterprise key={enterprise?.id} enterprise={enterprise} />
+					))}
+				</AppEnterprisesContainer>
+
+				<div className='load-more-btn'>
+					<Button buttonSize='xl' onClick={handleAddPageCount}>
+						Carregar mais
+					</Button>
+				</div>
+			</AppMainContainer>
 		</>
 	);
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-	let enterprises: Enterprise[] = [];
+	let enterprises: EnterpriseProps[] = [];
 
 	try {
-		const response: Response = await api.get('/enterprises');
+		const response: Response = await api.get('/enterprises?_limit=2&_page=1');
 		const enterprisesData = response.data;
 
 		enterprises = enterprisesData;
@@ -66,7 +96,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
 	return {
 		props: {
-			enterprises,
+			enterprises_list: enterprises,
 		},
 	};
 };
