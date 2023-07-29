3859fe1a5e2f79a15866da5cdd718e254d950640
OJailson17 <JaylsonO17@gmail.com>
2023-07-29 15:57:21 -0300

    fix: styled components not rendering on the server

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,44 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { AppType } from 'next/app';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+	// Is necessary to make styled components run in server side
+	static async getInitialProps(ctx: any) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App: AppType) => (props: AppType | any) =>
+						sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
